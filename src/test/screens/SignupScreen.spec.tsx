5019dc1a79bc9e96562c1fa081079d90cb7e0bb8
Tristan <tristan@tristansouthwell.com>
2020-04-29 21:43:09 +1000

    Testing packages installed and tests running

---
+++ src/test/screens/SignupScreen.spec.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, fireEvent } from 'react-native-testing-library';
+
+describe('SignupScreen', () => {
+  describe('test', () => {
+    const test = '';
+  });
+});
